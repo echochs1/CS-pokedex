9bd0607e5992373d8dc956ee19e9d00f4df205c5
cho-bani <echochs@bu.edu>
2023-11-07 09:36:40 -0500

    installed dev dependencies

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is my App component</h1>
+    </div>
+  )
+}
+
+// const root = createRoot(document.querySelector('#app'));
+// root.render(<App />,);
+
+export default App;
